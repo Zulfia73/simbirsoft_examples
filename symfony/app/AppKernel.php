db6818c3a61c50ee5ce00a2497af5be9d139b9a3
Dmitry Averbakh <dmitriy.averbah@simbirsoft.com>
2014-08-08 15:01:10 +0400

    Entity and fixture

--- symfony/app/AppKernel.php
+++ symfony/app/AppKernel.php
@@ -16,10 +16,13 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Acme\DemoBundle\AcmeDemoBundle(),
+            new Lexik\Bundle\FormFilterBundle\LexikFormFilterBundle(),
+            new JordiLlonch\Bundle\CrudGeneratorBundle\JordiLlonchCrudGeneratorBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
---
+++ symfony/src/Acme/DemoBundle/Controller/BookController.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Acme\DemoBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Pagerfanta\Pagerfanta;
+use Pagerfanta\Adapter\DoctrineORMAdapter;
+use Pagerfanta\View\TwitterBootstrapView;
+
+use Acme\DemoBundle\Entity\Book;
+use Acme\DemoBundle\Form\BookFilterType;
+
+/**
+ * Book controller.
+ *
+ * @Route("/book")
+ */
+class BookController extends Controller
+{
+    /**
+     * Lists all Book entities.
+     *
+     * @Route("/", name="book")
+     * @Method("GET")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        list($filterForm, $queryBuilder) = $this->filter();
+
+        list($entities, $pagerHtml) = $this->paginator($queryBuilder);
+
+        return array(
+            'entities' => $entities,
+            'pagerHtml' => $pagerHtml,
+            'filterForm' => $filterForm->createView(),
+        );
+    }
+
+    /**
+    * Create filter form and process filter request.
+    *
+    */
+    protected function filter()
+    {
+        $request = $this->getRequest();
+        $session = $request->getSession();
+        $filterForm = $this->createForm(new BookFilterType());
+        $em = $this->getDoctrine()->getManager();
+        $queryBuilder = $em->getRepository('AcmeDemoBundle:Book')->createQueryBuilder('e');
+
+        // Reset filter
+        if ($request->get('filter_action') == 'reset') {
+            $session->remove('BookControllerFilter');
+        }
+
+        // Filter action
+        if ($request->get('filter_action') == 'filter') {
+            // Bind values from the request
+            $filterForm->bind($request);
+
+            if ($filterForm->isValid()) {
+                // Build the query from the given form object
+                $this->get('lexik_form_filter.query_builder_updater')->addFilterConditions($filterForm, $queryBuilder);
+                // Save filter to session
+                $filterData = $filterForm->getData();
+                $session->set('BookControllerFilter', $filterData);
+            }
+        } else {
+            // Get filter from session
+            if ($session->has('BookControllerFilter')) {
+                $filterData = $session->get('BookControllerFilter');
+                $filterForm = $this->createForm(new BookFilterType(), $filterData);
+                $this->get('lexik_form_filter.query_builder_updater')->addFilterConditions($filterForm, $queryBuilder);
+            }
+        }
+
+        return array($filterForm, $queryBuilder);
+    }
+
+    /**
+    * Get results from paginator and get paginator view.
+    *
+    */
+    protected function paginator($queryBuilder)
+    {
+        // Paginator
+        $adapter = new DoctrineORMAdapter($queryBuilder);
+        $pagerfanta = new Pagerfanta($adapter);
+        $currentPage = $this->getRequest()->get('page', 1);
+        $pagerfanta->setCurrentPage($currentPage);
+        $entities = $pagerfanta->getCurrentPageResults();
+
+        // Paginator - route generator
+        $me = $this;
+        $routeGenerator = function($page) use ($me)
+        {
+            return $me->generateUrl('book', array('page' => $page));
+        };
+
+        // Paginator - view
+        $translator = $this->get('translator');
+        $view = new TwitterBootstrapView();
+        $pagerHtml = $view->render($pagerfanta, $routeGenerator, array(
+            'proximity' => 3,
+            'prev_message' => $translator->trans('views.index.pagprev', array(), 'JordiLlonchCrudGeneratorBundle'),
+            'next_message' => $translator->trans('views.index.pagnext', array(), 'JordiLlonchCrudGeneratorBundle'),
+        ));
+
+        return array($entities, $pagerHtml);
+    }
+
+    /**
+     * Finds and displays a Book entity.
+     *
+     * @Route("/{id}", name="book_show")
+     * @Method("GET")
+     * @Template()
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AcmeDemoBundle:Book')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Book entity.');
+        }
+
+        return array(
+            'entity'      => $entity,
+        );
+    }
+
+}
